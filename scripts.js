2461e12bf6199de978a0a88f8067fbe7698b74a4
allwrightgemma <34656602+allwrightgemma@users.noreply.github.com>
2023-01-23 13:52:21 +0000

    Added video effect filters

--- scripts.js
+++ scripts.js
@@ -25,6 +25,16 @@ function paintToCanvas() {
 
 	return setInterval(() => {
 		ctx.drawImage(video, 0, 0, width, height);
+		//take pixels out
+		let pixels = ctx.getImageData(0, 0, width, height);
+
+		//change the pixels
+		pixels = redEffect(pixels);
+		// pixels = rgbSplit(pixels);
+		// pixels = greenScreen(pixels);
+
+		//put pixels back in
+		ctx.putImageData(pixels, 0, 0);
 	}, 16); // runs function every 16ms. return so that you can call clear interval if you ever need to stop it
 }
 
@@ -40,6 +50,53 @@ function takePhoto() {
 	link.setAttribute('download', 'webcam photo');
 	link.innerHTML = `<img src="${data}" alt = "webcam photo" />`;
 	strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+	for (let i = 0; i < pixels.data.length; i += 4) {
+		pixels.data[i] = pixels.data[i] + 100; //red
+		pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
+		pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //blue
+	}
+	return pixels;
+}
+
+function rgbSplit(pixels) {
+	for (let i = 0; i < pixels.data.length; i += 4) {
+		pixels.data[i - 150] = pixels.data[i]; //red
+		pixels.data[i + 100] = pixels.data[i + 1]; //green
+		pixels.data[i - 150] = pixels.data[i + 2]; //blue
+	}
+	return pixels;
+}
+
+function greenScreen(pixels) {
+	const levels = {};
+
+	document.querySelectorAll('.rgb input').forEach((input) => {
+		levels[input.name] = input.value;
+	});
+
+	for (i = 0; i < pixels.data.length; i = i + 4) {
+		red = pixels.data[i + 0];
+		green = pixels.data[i + 1];
+		blue = pixels.data[i + 2];
+		alpha = pixels.data[i + 3];
+
+		if (
+			red >= levels.rmin &&
+			green >= levels.gmin &&
+			blue >= levels.bmin &&
+			red <= levels.rmax &&
+			green <= levels.gmax &&
+			blue <= levels.bmax
+		) {
+			// take it out!
+			pixels.data[i + 3] = 0;
+		}
+	}
+
+	return pixels;
 }
 
 getVideo();
